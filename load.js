d4779590c8d9880c6f542154aaf33c19f355acf2
sonneko <152277125+sonneko@users.noreply.github.com>
2024-06-29 20:32:07 +0900

    g

--- load.js
+++ load.js
@@ -4,14 +4,21 @@ const id = 1;
 //GASのURL
 const WebAppURL = "https://script.google.com/macros/s/AKfycbyWLs-2-sjRDVGIIK-QKENNOsM5j5R8DsjO3c1kuNvLvzl4UCm_pHr_f0x2Ldz8Qsw_/exec";
 
+//要素取得
+const contentElement = document.getElementById("content");
+const titleElement = document.getElementById("title")
+
 //fetchして雑学を取得
 const TargetURL = WebAppURL + "?id=" + id;
 let fetchDate;
 fetch(TargetURL)
-  .then(response => {
+  .then(function (date) {
     // レスポンス処理
-    return.date.json
+    return date.json()
   })
-  .catch(error => {
-    
+  .then(function (json){
+    //htmlにロード
+    const date = JSON.parse(json);
+    contentElement.innerHTML = date.html;
+    titleElement.innerHTML = date.title;
   });
